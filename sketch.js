98f11d754b71a1a7e75085bbde1968274d0d6f4c
Rodrigo Couto <r@rodg.co>
2023-01-03 14:33:52 -0300

    feat: Add Z dimension to perlin noise function

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 import { sketch } from "p5js-wrapper";
 
-// let rez1 = 0.01;
+let rez1 = 0.004;
 let rez2 = 0.008;
 // let rez3 = 0.006;
 let tam = 30;
@@ -8,6 +8,7 @@ let gap = -8;
 
 let lets = ["r", "o", "d", "r", "i", "g", "o"];
 
+let n1off = 0;
 let n2off = 0;
 
 sketch.setup = function () {
@@ -25,7 +26,7 @@ sketch.draw = function () {
   background(0, 0, 25);
   for (let x = 0; x < width + tam; x += tam + gap) {
     for (let y = 0; y < height + tam; y += tam + gap) {
-      const n2 = noise(x * rez2 + 10000 + n2off, y * rez2 + 10000 + n2off);
+      const n2 = noise(x * rez2 + 10000 + n2off, y * rez2 + 10000 + n2off, n1off);
       // const n3 = noise(x * rez1, y * rez1, n2off);
       fill(n2 * 360, 100, 90);
       push();
@@ -35,5 +36,6 @@ sketch.draw = function () {
       pop();
     }
   }
+  n1off += rez1;
   n2off += rez2;
 };
