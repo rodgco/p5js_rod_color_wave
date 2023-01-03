b6d273598d9c93f472f9923af5e2bd57cae940ec
Rodrigo Couto <r@rodg.co>
2023-01-03 14:28:12 -0300

    fix: Move to p5js-wrapper to fix module issue

--- sketch.js
+++ sketch.js
@@ -1,37 +1,39 @@
-rez1 = 0.01;
-rez2 = 0.008;
-rez3 = 0.006;
+import { sketch } from "p5js-wrapper";
+
+// let rez1 = 0.01;
+let rez2 = 0.008;
+// let rez3 = 0.006;
 let tam = 30;
 let gap = -8;
 
-let lets = ['r','o','d','r','i','g','o'];
+let lets = ["r", "o", "d", "r", "i", "g", "o"];
 
-n2off = 0;
+let n2off = 0;
 
-function setup() {
+sketch.setup = function () {
   createCanvas(windowWidth, windowHeight);
-  colorMode(HSB,360,120,100,255);
-  
+  colorMode(HSB, 360, 120, 100, 255);
+
   textSize(tam);
-}
+};
 
-function windowResized() {
+sketch.windowResized = function () {
   resizeCanvas(windowWidth, windowHeight);
-}
+};
 
-function draw() {
-  background(0,0,25);
-  for (x = 0; x<width+tam;x+=tam+gap){
-    for (y=0;y<height+tam;y+=tam+gap){
-      n2 = noise(x*rez2+10000+n2off,y*rez2+10000+n2off);
-      n3 = noise(x*rez1,y*rez1, n2off);
-      fill(n2*360,100,90);
+sketch.draw = function () {
+  background(0, 0, 25);
+  for (let x = 0; x < width + tam; x += tam + gap) {
+    for (let y = 0; y < height + tam; y += tam + gap) {
+      const n2 = noise(x * rez2 + 10000 + n2off, y * rez2 + 10000 + n2off);
+      // const n3 = noise(x * rez1, y * rez1, n2off);
+      fill(n2 * 360, 100, 90);
       push();
-      translate(x,y);
-      rotate((n2-0.5)*2*PI);
-      text(lets[(x+y)%7],0,0);
+      translate(x, y);
+      rotate((n2 - 0.5) * 2 * PI);
+      text(lets[(x + y) % 7], 0, 0);
       pop();
     }
   }
   n2off += rez2;
-}
+};
