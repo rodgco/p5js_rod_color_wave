50ef91de11c4970c77c62735fcd98d643e9d7db4
Rodrigo Couto <r@rodg.co>
2023-01-03 23:59:02 -0300

    feat: Use simplex-noise lib

--- sketch.js
+++ sketch.js
@@ -1,19 +1,22 @@
 import { sketch } from "p5js-wrapper";
+import { createNoise3D } from "simplex-noise";
 
-let rez1 = 0.004;
-let rez2 = 0.008;
+let rez1 = 0.008;
+let rez2 = 0.0008;
 // let rez3 = 0.006;
 let tam = 30;
 let gap = -7;
 
-let lets = ["r", "o", "d", "g", "c", "o"];
+let lets = "rodgco".split("");
 
 let n1off = 0;
 let n2off = 0;
 
+const noise3d = createNoise3D();
+
 sketch.setup = function () {
   createCanvas(windowWidth, windowHeight);
-  textFont('Prosto One');
+  textFont("Prosto One");
   colorMode(HSB, 360, 120, 100, 255);
   textSize(tam);
 };
@@ -26,12 +29,13 @@ sketch.draw = function () {
   background(0, 0, 25);
   for (let x = -10; x < width + tam; x += 26 + gap) {
     for (let y = -10; y < height + tam; y += 26 + gap) {
-      const n2 = noise(
-        x * rez2 + 10000 + n2off,
-        y * rez2 + 10000 + n2off,
-        n1off
+      const n2 = map(
+        noise3d(x * rez2 * 2 + n2off, y * rez2 * 2 + n2off, n1off),
+        -1,
+        1,
+        0,
+        1
       );
-      // const n3 = noise(x * rez1, y * rez1, n2off);
       fill(n2 * 360, 100, 90);
       push();
       translate(x, y);
